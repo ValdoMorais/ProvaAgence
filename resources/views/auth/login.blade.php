fdb728ee58c773368c6272b6f1d53aea5226aef7
Valdo Flor <82148347+ValdoMoraisFlor@users.noreply.github.com>
2022-11-05 21:02:46 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,8 +9,7 @@
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
+                        {{ csrf_field() }}
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
