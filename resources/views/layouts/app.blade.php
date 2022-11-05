ee369a182e1995faf9f3e9de76e621f7d895a656
Valdo Flor <82148347+ValdoMoraisFlor@users.noreply.github.com>
2022-11-05 21:40:19 +0200

    Chore: Melhorias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            @csrf
+                                            {{ csrf_field() }}
                                         </form>
                                     </li>
                                 </ul>
