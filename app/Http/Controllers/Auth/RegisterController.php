941bfb2db1c60324362dd0b25067dd8715a2b9e1
Valdo Flor <82148347+ValdoMoraisFlor@users.noreply.github.com>
2022-11-06 11:20:45 +0200

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,9 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+	protected $redirectTo = '/home';
+
+    //protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
