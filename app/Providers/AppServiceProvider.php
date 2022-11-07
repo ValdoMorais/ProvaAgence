9fafc8f8330621c994dc588f67213cab5ac649e2
Valdo Flor <82148347+ValdoMoraisFlor@users.noreply.github.com>
2022-11-07 11:51:06 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
+            // comente o statement a baixo se nao estiver em producao!
             URL:: forceScheme('https');
 
         
